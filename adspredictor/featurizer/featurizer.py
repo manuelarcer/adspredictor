6c4196fedaee577025c3340c6aa1f198ca8544b6
Juan <manuel.arceram@gmail.com>
2024-05-31 16:50:13 +0800

    fixed module and tests

--- adspredictor/featurizer/featurizer.py
+++ adspredictor/featurizer/featurizer.py
@@ -1,12 +1,36 @@
-
 import numpy as np
+import pandas as pd
 from ase.neighborlist import NeighborList, natural_cutoffs
 
 class AtomFeatures:
-    def __init__(self, atoms, natural_cutoff_factor=1):
+    def __init__(self, atoms, natural_cutoff_factor=1, cutoff_cn=11):
         self.atoms = atoms
         self.natural_cutoff_factor = natural_cutoff_factor
+        self.cutoff_cn = cutoff_cn
         self.neighbor_list = self.create_neighbor_list()
+        self.surface_atoms = self.get_surface_atoms()
+
+    def create_neighbor_list(self):
+        """
+        Create a NeighborList object for the atoms object.
+            natural_cutoff_factor: float
+        """
+        cutoffs = natural_cutoffs(self.atoms)
+        nl = NeighborList([c * self.natural_cutoff_factor for c in cutoffs], self_interaction=False, bothways=True)
+        nl.update(self.atoms)
+        return nl
+    
+    def get_surface_atoms(self):
+        """ 
+        Identify surface atoms based on coordination number. 
+            natural_cutoff_factor: float - factor to multiply the natural cutoffs by
+            cutoff_cn: int - coordination number cutoff for surface atoms
+        """
+        surface_atoms = []
+        for atom in self.atoms:
+            if len(self.neighbor_list.get_neighbors(atom.index)[0]) < self.cutoff_cn:
+                surface_atoms.append(atom.index)
+        return surface_atoms
     
     def get_atom_index(self, interest):
         """
@@ -24,91 +48,98 @@ class AtomFeatures:
             raise ValueError("Interest must be a string (atom symbol) or an integer (atom index).")
         return atom_index
     
-    def create_neighbor_list(self):
+    def determine_neigbors(self, interest, indexonly=True):
         """
-        Create a NeighborList object for the atoms object.
-            natural_cutoff_factor: float
+        interest: str or int
+            If str, the symbol of the atom of interest.
+            If int, the index of the atom of interest.
+        avoid: list of str
+            List of atom symbols to avoid. Useful for filtering adsorbate atoms.
         """
-        cutoffs = natural_cutoffs(self.atoms)
-        nl = NeighborList([c * self.natural_cutoff_factor for c in cutoffs], self_interaction=False, bothways=True)
-        nl.update(self.atoms)
-        return nl
+        index = self.get_atom_index(interest)
+        neigh_i, _ = self.neighbor_list.get_neighbors(index)
+        if indexonly:
+            return neigh_i
+        else:
+            neigh_symb = self.atoms[neigh_i].get_chemical_symbols()
+            return neigh_i, neigh_symb
     
-    def filter_neighbors(self, atom_index, avoid):
-        """
-        Filter the neighboring atoms of the atom of interest.
-            nl: NeighborList object
-            atom_index: int
-            atoms: list of ase.Atom objects
-            avoid: list of str
-                List of atom symbols to avoid. Useful for filtering adsorbate atoms.
-        """
-        indices, _ = self.neighbor_list.get_neighbors(atom_index)
-        filtered_indices = [i for i in indices if self.atoms[i].symbol not in avoid]
-        neighboring_atoms = [self.atoms[i].symbol for i in filtered_indices]
-        return neighboring_atoms, filtered_indices
-    
-    def find_neighboring_atoms(self, interest, avoid=[]):
-        atom_index = self.get_atom_index(interest)
-        neighboring_atoms, filtered_indices = self.filter_neighbors(atom_index, avoid)
-        return neighboring_atoms, filtered_indices
-
-    def get_surface_atoms(self, cutoff_cn=12):
-        """ 
-        Identify surface atoms based on coordination number. 
-            natural_cutoff_factor: float - factor to multiply the natural cutoffs by
-            cutoff_cn: int - coordination number cutoff for surface atoms
-        """
-        surface_atoms = []
-        for i, atom in enumerate(self.atoms):
-            if len(self.neighbor_list.get_neighbors(i)[0]) < cutoff_cn:
-                surface_atoms.append(i)
-        return surface_atoms
-
 class FeatureCreator:
-    def __init__(self, df):
-        self.df = df
+    def __init__(self, df, ads, listmetals):
+        self.df = df       # DataFrame with 'Atoms' column
+        self.ads = ads
+        self.listmetals = listmetals
+        self.bindingsites_idx = self.bindingsites_indexes()
+        self.bindingsites_symb = self.bindingsites_symbols()
 
-    def binding_atoms_per_type(self, atoms, ads, metaltype):
-        """
-        Count the number of specified metal atoms bonded to the adsorbate atom.
-            ads: str or int - symbol or index of the adsorbate atom
-            metaltype: str - symbol of the metal atom
-        """
-        af = AtomFeatures(atoms)
-        binding_metals, _ = af.find_neighboring_atoms(interest=ads, avoid=[])
-        count = len([m for m in binding_metals if m == metaltype])
-        return count
+#    def neigbors_serie(self, interest):
+#        # interest: str (Symbol) or int (Index) of atom of interest
+#        return self.df['Atoms'].apply(lambda x: AtomFeatures(x).determine_neigbors(interest, indexonly=False))
+
+    def bindingsites_indexes(self):
+        serie = self.df['Atoms'].apply(lambda x: AtomFeatures(x).determine_neigbors(self.ads, indexonly=False))
+        return serie.apply(lambda x: x[0])
+
+    def bindingsites_symbols(self):
+        serie = self.df['Atoms'].apply(lambda x: AtomFeatures(x).determine_neigbors(self.ads, indexonly=False))
+        return serie.apply(lambda x: x[1])
+
+    def create_feature_binding_site(self):
+        for metal in self.listmetals:
+            self.df[f'bonding_{metal}'] = self.bindingsites_symb.apply(lambda x: count_atoms_x_type(x, metal))
+        return self.df
+
+    def second_neighbors(self):
+        dummy_df = pd.DataFrame(self.bindingsites_idx)
+        second_neigh_serie = dummy_df.apply(
+                lambda x: [find_neigh(self.df.Atoms.iloc[x.name], i, avoid=[self.ads]) for i in x.iloc[0]], axis=1)
+        # Combine the lists in each row inside second_neighbors and then remove duplicates
+        ## Needed for x-fold type of adsorption
+        second_neigh_serie = second_neigh_serie.apply(lambda x: [item for sublist in x for item in sublist])
+        second_neigh_serie = second_neigh_serie.apply(lambda x: list(set(x)))
+        second_neigh_serie = pd.DataFrame(second_neigh_serie)
+        second_neigh_serie = second_neigh_serie.apply(
+                lambda x: [i for i in x[0] if i not in self.bindingsites_idx.iloc[x.name]], axis=1
+                )
+        return second_neigh_serie
     
-    def neighbor_count_per_type(self, atoms, ads, metal):
-        """
-        Count the number of specified metal atoms neighboring the adsorbate atom's neighbors.
-            ads: str or int - symbol or index of the adsorbate atom
-            metal: str - symbol of the metal atom
-        """
-        af = AtomFeatures(atoms)
-        _, bm_indices = af.find_neighboring_atoms(interest=ads, avoid=[])
-        neigh_indexes = []
-        metal_symb = []
-        for i in bm_indices:
-            mneigh, mn_indices = af.find_neighboring_atoms(interest=i, avoid=[ads])
-            for j, index in enumerate(mn_indices):
-                if index not in neigh_indexes and index not in bm_indices:
-                    neigh_indexes.append(index)
-                    metal_symb.append(mneigh[j])
-        count = len([m for m in metal_symb if m == metal])
-        return count
+    def create_feature_second_neighbors(self, distinguishsurface=False):
+        second_neigh_serie = self.second_neighbors()
+        second_neigh_serie = pd.DataFrame(second_neigh_serie)
+        # Count the number of each atom type in the list
+        if distinguishsurface:
+            symbols_surf = second_neigh_serie.apply(lambda x: [
+                        self.df.Atoms.iloc[x.name][i].symbol for i in x.iloc[0] if i in AtomFeatures(self.df.Atoms.iloc[x.name]).get_surface_atoms()
+                        ], axis=1)
+            symbols_bulk = second_neigh_serie.apply(lambda x: [
+                        self.df.Atoms.iloc[x.name][i].symbol for i in x.iloc[0] if i not in AtomFeatures(self.df.Atoms.iloc[x.name]).get_surface_atoms()
+                        ], axis=1)
+            for metal in self.listmetals:
+                self.df[f'neigh_surf_{metal}'] = symbols_surf.apply(lambda x: count_atoms_x_type(x, metal))
+                self.df[f'neigh_bulk_{metal}'] = symbols_bulk.apply(lambda x: count_atoms_x_type(x, metal))
+        else:
+            symbols = second_neigh_serie.apply(lambda x: [
+                        self.df.Atoms.iloc[x.name][i].symbol for i in x.iloc[0]
+                        ], axis=1)
+            for metal in self.listmetals:
+                self.df[f'neigh_{metal}'] = symbols.apply(lambda x: count_atoms_x_type(x, metal))
+        
+        return self.df
 
-    def add_bonding_features(self, metals=['Ag', 'Au', 'Cu', 'Pd', 'Pt'], ads='N'):
-        """
-        Add bonding features to the dataframe for specified metals.
-        """
-        for metal in metals:
-            self.df[f'bonding_{metal}'] = self.df.apply(lambda x: self.binding_atoms_per_type(x.Atoms, ads=ads, metaltype=metal), axis=1)
+def count_atoms_x_type(listsymbols, metalsymb, avoid=[]):
+    # listsymbols: list of str (List of atom symbols)
+    # metalsymb: str (Symbol of metal atom for feature)
+    # avoid: list of str (List of atom symbols to avoid)
 
-    def add_neigh_features(self, metals=['Ag', 'Au', 'Cu', 'Pd', 'Pt'], ads='N'):
-        """
-        Add neighboring features to the dataframe for specified metals.
-        """
-        for metal in metals:
-            self.df[f'neigh_{metal}'] = self.df.apply(lambda x: self.neighbor_count_per_type(x.Atoms, ads=ads, metal=metal), axis=1)
+    # How many items in neigh_symb match metalsymb
+    count = sum([1 for symb in listsymbols if symb == metalsymb and symb not in avoid])
+    return count
+
+def find_neigh(atoms, interest, avoid=[]):
+    # atoms: Atoms object
+    # interest: str or int (Symbol or index of the atom of interest)
+    af = AtomFeatures(atoms)
+    index_to_avoid = [af.get_atom_index(a) for a in avoid]
+    all_neigh = af.determine_neigbors(interest)
+    # remove avoid indexes
+    return [i for i in all_neigh if i not in index_to_avoid]
